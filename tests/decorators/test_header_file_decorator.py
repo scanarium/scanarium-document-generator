3034462d8d42e78289a6324dc8749914a45edeed
Christian Aistleitner <christian@quelltextlich.at>
2021-05-17 11:28:29 +0200

    Adjust tests for added class in Header Decorator

--- tests/decorators/test_header_file_decorator.py
+++ tests/decorators/test_header_file_decorator.py
@@ -13,7 +13,8 @@ class HeaderFileDecoratorTest(DocumentPageTestCase):
         decorator = HeaderFileDecorator()
         decorator.decorate_file(file, decorator.init_state())
 
-        self.assertEqual(file['markdown'], '# foo {: #foo}')
+        self.assertEqual(file['markdown'],
+                         '# foo {: #foo class=document-generator-node}')
 
     def test_plain(self):
         file = {
@@ -25,7 +26,9 @@ class HeaderFileDecoratorTest(DocumentPageTestCase):
         decorator = HeaderFileDecorator()
         decorator.decorate_file(file, decorator.init_state())
 
-        self.assertEqual(file['markdown'], '### Title foo {: #idFoo}\n\nbar')
+        self.assertEqual(file['markdown'],
+                         '### Title foo {: #idFoo '
+                         'class=document-generator-node}\n\nbar')
 
     def test_attributes_already_present(self):
         file = {
@@ -38,7 +41,8 @@ class HeaderFileDecoratorTest(DocumentPageTestCase):
         decorator.decorate_file(file, decorator.init_state())
 
         self.assertEqual(file['markdown'],
-                         '# Title foo {: #idFoo key=value}\n\nbar')
+                         '# Title foo {: #idFoo class=document-generator-node '
+                         'key=value}\n\nbar')
 
     def test_attributes_already_open_no_end(self):
         file = {
@@ -51,4 +55,5 @@ class HeaderFileDecoratorTest(DocumentPageTestCase):
         decorator.decorate_file(file, decorator.init_state())
 
         self.assertEqual(file['markdown'],
-                         '# Title foo {: #idFoo key=value}\n\nbar')
+                         '# Title foo {: #idFoo class=document-generator-node '
+                         'key=value}\n\nbar')
--- tests/test_root_generator.py
+++ tests/test_root_generator.py
@@ -26,12 +26,14 @@ class DocumentGeneratorTest(DocumentPageTestCase):
                 contents = f.read()
             self.assertIn('<html', contents)
             self.assertIn('Root node', contents)
-            self.assertIn('<h3 id="chapter11">Chapter 11', contents)
+            self.assertIn('<h3 class="document-generator-node" id="chapter11">'
+                          'Chapter 11', contents)
             self.assertIn('Chapter 111', contents)
 
             with open(os.path.join(dir, 'all.html.de')) as f:
                 contents = f.read()
             self.assertIn('<html', contents)
-            self.assertIn('<h3 id="chapter11">Kapitel 11', contents)
+            self.assertIn('<h3 class="document-generator-node" id="chapter11">'
+                          'Kapitel 11', contents)
             self.assertIn('Kapitel 111', contents)
             self.assertIn('Chapter 2', contents)
