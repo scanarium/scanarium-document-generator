595432a3643c216cbf7426d1f0b589eb67074dcc
Christian Aistleitner <christian@quelltextlich.at>
2021-05-17 22:49:14 +0200

    Decorators: Allow to flag warnings/errors
    This will allow to give better error messages

--- document_generator/decorators/decorator.py
+++ document_generator/decorators/decorator.py
@@ -1,6 +1,31 @@
 class Decorator(object):
     def init_state(self):
-        return {}
+        return {
+            '__decorator': {
+                'messages': [],
+                }
+            }
+
+    def _add_message(self, kind, state, text):
+        message = {
+            'decorator': self,
+            'kind': kind,
+            'node': state['__decorator']['node'],
+            'text': text,
+            }
+        if 'file' in state['__decorator']:
+            message['file'] = state['__decorator']['file']
+
+        state['__decorator']['messages'].append(message)
+
+    def add_warning(self, state, text):
+        self._add_message('warning', state, text)
+
+    def add_error(self, state, text):
+        self._add_message('error', state, text)
+
+    def get_messages(self, state):
+        return state['__decorator']['messages']
 
     def decorate_file(self, file, state):
         pass
@@ -11,13 +36,29 @@ class Decorator(object):
     def decorate_node_exit(self, node, state):
         pass
 
+    def call_guarded(self, func, obj, state):
+        try:
+            func(obj, state)
+        except Exception as e:
+            self.add_error(state, f'Caught exception {e}')
+
     def run(self, node, state={}):
-        self.decorate_node_enter(node, state)
+        state['__decorator']['node'] = node
+        self.call_guarded(self.decorate_node_enter, node, state)
 
         for file_key in sorted(node['files'].keys()):
-            self.decorate_file(node['files'][file_key], state)
+            file = node['files'][file_key]
+            state['__decorator']['file'] = file
+            self.call_guarded(self.decorate_file, file, state)
+
+        try:
+            del state['__decorator']['file']
+        except KeyError:
+            # Node does not have files. Nothing to clean up.
+            pass
 
         for subnode in node['subnodes']:
-            self.run(subnode, state)
+            self.call_guarded(self.run, subnode, state)
+            state['__decorator']['node'] = node
 
-        self.decorate_node_exit(node, state)
+        self.call_guarded(self.decorate_node_exit, node, state)
--- tests/decorators/test_decorator.py
+++ tests/decorators/test_decorator.py
@@ -6,6 +6,15 @@ class StubDecorator(Decorator):
     def __init__(self):
         super().__init__()
         self.visited = []
+        self.warn_on_enter = 0
+        self.warn_on_file = 0
+        self.warn_on_exit = 0
+        self.error_on_enter = 0
+        self.error_on_file = 0
+        self.error_on_exit = 0
+        self.raise_on_enter = 0
+        self.raise_on_file = 0
+        self.raise_on_exit = 0
 
     def init_state(self):
         state = super().init_state()
@@ -17,12 +26,30 @@ class StubDecorator(Decorator):
 
     def decorate_node_enter(self, node, state):
         state['number'] += 1
+        if (state['number'] == self.warn_on_enter):
+            self.add_warning(state, f'node-enter {node["name"]}')
+        if (state['number'] == self.error_on_enter):
+            self.add_error(state, f'node-enter {node["name"]}')
+        if (state['number'] == self.raise_on_enter):
+            raise RuntimeError(f'node-enter {node["name"]}')
         self.mark_visited('node-enter', node['name'], state)
 
     def decorate_file(self, file, state):
+        if (state['number'] == self.warn_on_file):
+            self.add_warning(state, f'file {file["markdown"]}')
+        if (state['number'] == self.error_on_file):
+            self.add_error(state, f'file {file["markdown"]}')
+        if (state['number'] == self.raise_on_file):
+            raise RuntimeError(f'file {file["markdown"]}')
         self.mark_visited('file', file['markdown'], state)
 
     def decorate_node_exit(self, node, state):
+        if (state['number'] == self.warn_on_exit):
+            self.add_warning(state, f'node-exit {node["name"]}')
+        if (state['number'] == self.error_on_exit):
+            self.add_error(state, f'node-exit {node["name"]}')
+        if (state['number'] == self.raise_on_exit):
+            raise RuntimeError(f'node-exit {node["name"]}')
         self.mark_visited('node-exit', node['name'], state)
         state['number'] -= 1
 
@@ -82,3 +109,203 @@ class DecoratorTest(DocumentPageTestCase):
                 "node-exit-node12 (state: 44)",
                 "node-exit-node1 (state: 43)",
                 ])
+
+        messages = decorator.get_messages(state)
+        self.assertEqual(len(messages), 0)
+
+    def test_warning_enter(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.warn_on_enter = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertEqual(messages[0]['text'], 'node-enter node1')
+        self.assertEqual(messages[0]['kind'], 'warning')
+        self.assertEqual(len(messages), 1)
+
+    def test_warning_exit(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.warn_on_exit = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertEqual(messages[0]['text'], 'node-exit node1')
+        self.assertEqual(messages[0]['kind'], 'warning')
+        self.assertEqual(len(messages), 1)
+
+    def test_warning_file(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+                'en': {
+                    'markdown': 'en',
+                    },
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.warn_on_file = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertEqual(messages[0]['text'], 'file en')
+        self.assertEqual(messages[0]['kind'], 'warning')
+        self.assertEqual(len(messages), 1)
+
+    def test_error_enter(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.error_on_enter = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertEqual(messages[0]['text'], 'node-enter node1')
+        self.assertEqual(messages[0]['kind'], 'error')
+        self.assertEqual(len(messages), 1)
+
+    def test_error_exit(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.error_on_exit = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertEqual(messages[0]['text'], 'node-exit node1')
+        self.assertEqual(messages[0]['kind'], 'error')
+        self.assertEqual(len(messages), 1)
+
+    def test_error_file(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+                'en': {
+                    'markdown': 'en',
+                    },
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.error_on_file = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertEqual(messages[0]['text'], 'file en')
+        self.assertEqual(messages[0]['kind'], 'error')
+        self.assertEqual(len(messages), 1)
+
+    def test_warning_error_mix(self):
+        node11 = {
+            'name': 'node11',
+            'files': {
+                'de': {
+                    'markdown': 'de',
+                    },
+            },
+            'subnodes': [],
+        }
+        node1 = {
+            'name': 'node1',
+            'files': {
+                'en': {
+                    'markdown': 'en',
+                    },
+            },
+            'subnodes': [node11],
+        }
+        decorator = StubDecorator()
+        decorator.warn_on_enter = 44
+        decorator.error_on_file = 43
+        decorator.warn_on_exit = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertEqual(messages[0]['text'], 'file en')
+        self.assertEqual(messages[0]['kind'], 'error')
+        self.assertEqual(messages[1]['text'], 'node-enter node11')
+        self.assertEqual(messages[1]['kind'], 'warning')
+        self.assertEqual(messages[2]['text'], 'node-exit node1')
+        self.assertEqual(messages[2]['kind'], 'warning')
+        self.assertEqual(len(messages), 3)
+
+    def test_raise_enter(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.raise_on_enter = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertIn('node-enter node1', messages[0]['text'])
+        self.assertEqual(messages[0]['kind'], 'error')
+        self.assertEqual(len(messages), 1)
+
+    def test_raise_exit(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.raise_on_exit = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertIn('node-exit node1', messages[0]['text'])
+        self.assertEqual(messages[0]['kind'], 'error')
+        self.assertEqual(len(messages), 1)
+
+    def test_raise_file(self):
+        node1 = {
+            'name': 'node1',
+            'files': {
+                'en': {
+                    'markdown': 'en',
+                    },
+            },
+            'subnodes': [],
+        }
+        decorator = StubDecorator()
+        decorator.raise_on_file = 43
+        state = decorator.init_state()
+        decorator.run(node1, state)
+
+        messages = decorator.get_messages(state)
+        self.assertIn('file en', messages[0]['text'])
+        self.assertEqual(messages[0]['kind'], 'error')
+        self.assertEqual(len(messages), 1)
